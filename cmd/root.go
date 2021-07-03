bbaad4e11880b0513b993531e078df459575322b
skanehira <sho19921005@gmail.com>
2021-07-03 22:40:16 +0900

    Fix config init

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/skanehira/remonade/config"
 	"github.com/skanehira/remonade/ui"
 	"github.com/skanehira/remonade/util"
 	"github.com/spf13/cobra"
@@ -12,6 +18,20 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	rootCmd.Run = func(cmd *cobra.Command, args []string) {
+		config.Load()
+		debug := os.Getenv("DEBUG")
+		if debug != "" {
+			path := filepath.Join(filepath.Dir(config.Path), "debug.log")
+			f, err := os.OpenFile(path,
+				os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+			if err != nil {
+				util.ExitError(err)
+			}
+			defer f.Close()
+			log.SetOutput(f)
+		} else {
+			log.SetOutput(ioutil.Discard)
+		}
 		ui.Start()
 	}
 
